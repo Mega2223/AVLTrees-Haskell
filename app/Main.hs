7b7521caca93d64e6daea5e2078ebfb96c41c71c
mega2223 <juliocesarborgesdeoliveir@gmail.com>
2026-08-04 23:59:18 -0300

    uau

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,44 @@
 f :: Int -> Int
 f x = if x == 0 || x == 1 then x else f(x-1) + f(x-2)  
 
-treeFrom :: ([Char],[(Char,Char)]) -> ([Char],[(Char,Char)])
-treeFrom graph = do
-	let g = ([],[])
+data Record = Record {value :: Int, name :: [Char]}
+data Node = Node {label :: [Char], record :: Record}
+data Arrow = Arrow {begin :: Node, end :: Node}
+data Digraph = Digraph {nodes :: [Node], arrows :: [Arrow]}
+
+showNode :: Node -> String
+showNode = label
+
+showArrow :: Arrow -> String
+showArrow arrow = showNode (begin arrow) ++ "," ++ showNode (end arrow)
+
+showDg :: Digraph -> String
+showDg digraph = do
+	let compNodes nds = do 
+		if null nds then
+			"?"
+		else
+			showNode(head nds) ++ "," ++ compNodes(tail nds)
+
+	let compArrows ars = do
+		if null ars then 
+			"|"
+		else
+			showArrow(head ars) ++ "," ++ compArrows(tail ars)
+	compNodes (nodes digraph) ++ "\n" ++ compArrows (arrows digraph)
+
+treeBFS :: Digraph -> Node -> [Node] -> Digraph
+treeBFS digraph node discoveredNodes = do
+	let g = Digraph [] []
 	g
 
 main :: IO ()
 main = do
-	let v = [f x | x <- [1..30]]
+	let drecords = [Record i ("T"++ show i) | i <- [1..20]]
+	let dnodes = [Node (name recd) recd | recd <- drecords]
+	let darrows = []
+	let digraph = Digraph dnodes darrows
+	let start = head dnodes
+	let res = treeBFS digraph start []
 	
-	print (show v)
+	print (showDg digraph)

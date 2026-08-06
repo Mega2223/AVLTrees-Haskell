944fc4a43eeb5d8d9410206005d2402a3476a88d
mega2223 <juliocesarborgesdeoliveir@gmail.com>
2026-08-05 21:01:41 -0300

    rebalanceamentos

--- app/Tree.hs
+++ app/Tree.hs
@@ -1,4 +1,3 @@
-
 data Node = Node {left :: Node, right :: Node, val :: Int} | Void
 
 printTree :: Node -> Int -> String
@@ -9,12 +8,81 @@ printTree Void _ = do
 	"N"
 
 insertNoBalance :: Node -> Node -> Node
+insertNoBalance Void node = node
 insertNoBalance (Node ll rr vv) node = do
 	if val node < vv then
 		Node (insertNoBalance ll node) rr vv
 	else
 		Node ll (insertNoBalance rr node) vv
-insertNoBalance Void node = node
+
+height :: Node -> Int
+height Void = 0
+height (Node ll rr _) = 1 + max (height ll) (height rr)
+
+balance :: Node -> Int
+balance Void = 0
+balance (Node ll rr _) = height ll - height rr
+
+rotateR :: Node -> Node
+rotateR Void = Void
+rotateR (Node ll rr vv) = do
+	let x = Node ll rr vv
+	let y = ll
+	let a = left y
+	let b = right y
+	let c = rr
+	Node a (Node b c vv) (val y)
+
+rotateL :: Node -> Node
+rotateL Void = Void
+rotateL (Node ll rr vv) = do
+	let x = Node ll rr vv
+	let y = rr
+	let c = ll 
+	let b = left y
+	let a = right y
+	Node (Node c b (val x)) a (val y)
+
+rotateLR :: Node -> Node
+rotateLR Void = Void
+rotateLR (Node ll rr vv) = do
+	let x = Node ll rr vv
+	let y = left x
+	let z = right y
+	let yy = rotateL y
+	let xx = rotateR (Node yy (right x) (val x))
+	xx
+
+rotateRL :: Node -> Node
+rotateRL Void = Void
+rotateRL (Node ll rr vv) = do
+	let x = Node ll rr vv
+	let y = right x
+	let z = left y
+	let yy = rotateR y
+	let xx = rotateL (Node (left x) yy (val x))
+	xx
+
+rebalance :: Node -> Node
+rebalance Void = Void
+rebalance (Node ll rr vv) = do
+	let selfN = Node ll rr vv
+
+	let bal = balance selfN
+
+	let llr = rebalance ll
+	let rrl = rebalance rr
+	
+	selfN
+
+insertKeepBalance :: Node -> Node -> Node
+insertKeepBalance Void node = node
+insertKeepBalance (Node ll rr vv) node = do
+	let node' = insertNoBalance (Node ll rr vv) node
+
+	node'
+
+-- TODO função printflat
 
 main :: IO ()
 main = do
